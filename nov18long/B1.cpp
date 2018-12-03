9888a40c464f4a469e9868a9bf5837fb8d735b34
Frkymhn <panwarmahendra6@gmail.com>
2018-12-03 22:07:09 +0530

    solved first two problems today

---
+++ nov18long/B1.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(int _=b,i=a;i<_;++i)
+#define ROF(i,b,a) for(int _=a,i=b;i>_;--i)
+#define REP(n) for(int _=(n);_--;)
+#define _1 first
+#define _2 second
+#define PB push_back
+#define SZ(x) int((x).size())
+#define ALL(x) begin(x),end(x)
+#define MSET(m,v) memset(m,v,sizeof(m))
+#define MAX_PQ(T) priority_queue<T>
+#define MIN_PQ(T) priority_queue<T,vector<T>,greater<T>>
+#define IO {ios_base::sync_with_stdio(0);cin.tie(0);}
+#define nl '\n'
+#define cint1(a) int a;cin>>a
+#define cints(a) string a;cin>>a
+#define cint2(a,b) int a,b;cin>>a>>b
+#define cint3(a,b,c) int a,b,c;cin>>a>>b>>c
+
+int main()
+{
+	cint1(T);
+	while(T--)
+	{
+		cint2(a,b);
+		cout<<"YES\n";
+	}
+	return 0;
+}
